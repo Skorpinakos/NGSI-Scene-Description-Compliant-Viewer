185d191ee61d34e45e1dc529db0d448f0cf8db87
Ioannis Tsampras <gtsambras@gmail.com>
2025-04-03 02:31:29 +0300

    the slight restructure

---
+++ tools/pcd_point_counter.py
@@ -0,0 +1,10 @@
+import open3d as o3d
+
+def print_pointcloud_count(ply_file):
+    pcd = o3d.io.read_point_cloud(ply_file)
+    points = len(pcd.points)
+    print(f"Point count: {points}")
+
+if __name__ == "__main__":
+    ply_file = "background_assets/small_parking.ply"
+    print_pointcloud_count(ply_file)
